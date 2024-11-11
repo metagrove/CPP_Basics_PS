9d5420aa002a4ff67b9a73cef67bd63ec3ce8d01
tarun <echogrove.acc@gmail.com>
2024-11-12 01:23:38 +0530

    sort.py

--- Basics/sort.py
+++ Basics/sort.py
@@ -1,6 +1,7 @@
 arr = [1,4,3,2,4,2,1,5,7]
 
-for i in arr:
-    for j in range (i,-1):
-       if arr[i]>arr[j]:
+for i in range(0,len(arr)):
+    for j in range (i,0,-1):
+       if arr[i]>arr[j]
+        arr[i],arr[j] = arr[j],arr[i]
             print(i)
